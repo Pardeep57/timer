d0cc942b1d7939ede0868cea18c19b2efb5aa687
Pardeep57 <pardeepofficial57@gmail.com>
2022-08-31 07:39:10 +0000

    timer function

---
+++ timer1.js
@@ -0,0 +1,14 @@
+//const sentence = "hello there from lighthouse labs";
+
+   const args = process.argv.slice(2).map(num => Number(num));
+
+  const timer = args => {
+  args.forEach(num => {
+    if (num > 0 && num ) {
+      setTimeout(() => console.log("Alarm", "\x07"), num * 1000);
+    }
+  });
+};
+
+timer(args);
+
